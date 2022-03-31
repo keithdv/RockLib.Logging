06bcc3a320038b1327c0d98efad5a2e7cb8c788f
Keith Voels <keithvoels@quickenloans.com>
2022-03-31 17:14:34 -0500

    Highlight missing correlation id

--- Examples/Example.Logging.AspNetCore/Controllers/WeatherForecastController.cs
+++ Examples/Example.Logging.AspNetCore/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using RockLib.Logging.AspNetCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using RockLib.Logging;
 
 namespace Example.Logging.AspNetCore.Controllers
 {
@@ -18,15 +19,28 @@ namespace Example.Logging.AspNetCore.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, RockLib.Logging.ILogger rockLibLogger)
         {
             _logger = logger;
+            RockLibLogger = rockLibLogger;
         }
+
+        public RockLib.Logging.ILogger RockLibLogger { get; }
 
         [HttpGet]
         [InfoLog]
         public IEnumerable<WeatherForecast> Get()
         {
+            // Now, you would never use these next to each other but this highlights the issue
+
+            // Overriding the default logger using a custom Logging Provider in Program
+            // I would expect these two loggers to use the same underlying RockLib.Logger.ILogger
+            // and get the same correlation id
+            // But they do not
+
+            _logger.LogInformation("Microsoft Logger");
+            RockLibLogger.Info("RockLib Logger");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- Examples/Example.Logging.AspNetCore/Program.cs
+++ Examples/Example.Logging.AspNetCore/Program.cs
@@ -2,10 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RockLib.Logging;
 
 namespace Example.Logging.AspNetCore
 {
@@ -21,6 +18,10 @@ namespace Example.Logging.AspNetCore
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.AddRockLibLoggerProvider();
                 });
     }
 }
